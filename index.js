264b7a99b3f29e0281cbd8fb8af21ccf71f16767
Jakub Zedník <91053389+kuboi1@users.noreply.github.com>
2023-01-09 16:15:27 +0100

    finish up

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const toggled = {
 }
 
 let gameOver = false
+let gameWon = false
 
 let guessNodes = [firstGuessNode]
 let activeGuessIndex = 0
@@ -25,25 +26,34 @@ let activeGuess = firstGuessNode
 let activeGuessLetters = firstGuessNode.querySelectorAll('.guess-letter')
 
 let guessWord = ''
+let guessedWords = []
 
+let localData
 let wordList
-let wordOfTheDay
 
 function init() {
+    // Load local data
+    initLocalData()
+
+    console.log(localData.wordOfTheDay)
+    
     // Create guess containers
     initGuessContainers()
-
+    
     // Setup UI keyboard click listeners
     initUIKeyboard()
-
+    
     // Setup keydown listener
     initKeyDown()
-
+    
     // Setup modal windows
     initModals()
 
-    loadWordList()
-
+    // Setup options buttons
+    initOptions()
+    
+    setupGame()
+    
 }
 
 function initGuessContainers() {
@@ -55,19 +65,24 @@ function initGuessContainers() {
 }
 
 function initUIKeyboard() {
-    keyboardKeys.forEach((keyboardLetter) => {
-        if (!keyboardLetter.dataset.special) {
-            keyboardLetter.dataset.letter = keyboardLetter.textContent
+    keyboardKeys.forEach((keyboardKey) => {
+        if (!keyboardKey.dataset.special) {
+            keyboardKey.dataset.letter = keyboardKey.textContent
         }
-        if (keyboardLetter.dataset.toggle) {
-            const key = keyboardLetter.dataset.toggleKey
-            const toggleLetter = keyboardLetter.dataset.toggle
-            toggleMap[key][toggleLetter] = keyboardLetter
+        if (keyboardKey.dataset.toggle) {
+            const key = keyboardKey.dataset.toggleKey
+            const toggleLetter = keyboardKey.dataset.toggle
+            if (key == 3) {
+                toggleMap[1][toggleLetter[0]] = keyboardKey
+                toggleMap[2][toggleLetter[1]] = keyboardKey
+            } else {
+                toggleMap[key][toggleLetter] = keyboardKey
+            }
         }
 
-        keyboardLetter.addEventListener('click', () => {
-            if (keyboardLetter.dataset.special) {
-                switch (keyboardLetter.dataset.special) {
+        keyboardKey.addEventListener('click', () => {
+            if (keyboardKey.dataset.special) {
+                switch (keyboardKey.dataset.special) {
                     case 'backspace':
                         eraseLetter()
                         return
@@ -75,14 +90,14 @@ function initUIKeyboard() {
                         makeGuess()
                         return
                     case 'toggle':
-                        toggleLetters(keyboardLetter.dataset.toggleKey)
+                        toggleLetters(keyboardKey.dataset.toggleKey)
                         return
                     default:
                         return
                 }
             }
 
-            writeLetter(keyboardLetter.textContent)
+            writeLetter(keyboardKey.textContent)
         })
     })
 }
@@ -125,13 +140,76 @@ function closeAllModals() {
     })
 }
 
-function loadWordList() {
+function updateStatsModal() {
+    const statsModal = document.querySelector('[data-modal="stats"]')
+    statsModal.querySelector('.end-modal-title').textContent = gameOver ? (gameWon ? 'DOBRÁ PRÁCE!' : 'Dnes to bohužel nevyšlo...') : 'Dnešní hru jste ještě nedohrál/a.'
+    statsModal.querySelector('.end-modal-subtitle').classList.toggle('hidden', !gameOver)
+    statsModal.querySelector('.end-modal-wordoftheday').textContent = localData.wordOfTheDay.toUpperCase()
+    statsModal.querySelector('[data-stat="streak"]').textContent = localData.streak
+    statsModal.querySelector('[data-stat="guess-today"]').textContent = guessedWords.length
+    statsModal.querySelector('[data-stat="guess-average"]').textContent = (localData.guessCount / localData.gameCount).toFixed(1)
+}
+
+function initLocalData() {
+    localData = window.localStorage.getItem('wordleData')
+
+    console.log(localData)
+
+    if (localData) {
+        localData = JSON.parse(localData)
+    } else {
+        localData = {
+            finishedToday: false,
+            wordOfTheDay: null,
+            gameState: [],
+            streak: 0,
+            guessCount: 0,
+            gameCount: 0,
+            lastSave: null
+        }
+        saveLocalData()
+    }
+}
+
+function loadGameState() {
+    for (const word of localData.gameState) {
+        for (const letter of word) {
+            writeLetter(letter.toUpperCase())
+        }
+        makeGuess()
+    }
+}
+
+function saveLocalData() {
+    localData.lastSave = getCurrentDate()
+    window.localStorage.setItem('wordleData', JSON.stringify(localData))
+}
+
+function initOptions() {
+    const optionsContainer = document.querySelector('.options-container')
+
+    const statsButton = optionsContainer.querySelector('[data-options="stats"]')
+
+    statsButton.addEventListener('click', (e) => {
+        updateStatsModal()
+        toggleModal('stats')
+    })
+}
+
+function setupGame() {
     fetch('./data/word_list.json')
     .then(
         function(response) {
             response.json().then(function(data) {
                 wordList = data['words']
-                pickWordOfTheDay()
+                // If word of the day was not set or it is a new day
+                if (!localData.wordOfTheDay || getCurrentDate() !== localData.lastSave) {
+                    localData.finishedToday = false
+                    localData.gameState = []
+                    pickWordOfTheDay()
+                    saveLocalData()
+                }
+                loadGameState()
             })
         }
     )
@@ -142,8 +220,12 @@ function loadWordList() {
 
 function pickWordOfTheDay() {
     var randomIndex = Math.floor(Math.random() * wordList.length)
-    wordOfTheDay = wordList[randomIndex]
-    console.log(wordOfTheDay)
+    localData.wordOfTheDay = wordList[randomIndex]
+}
+
+function getCurrentDate() {
+    const date = new Date()
+    return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
 }
 
 function toggleModal(key) {
@@ -205,9 +287,22 @@ function toggleLetters(key) {
     toggleKeys.forEach((k) => {
         if (k.dataset.toggleKey == key) {
             k.classList.toggle('toggled')
+        } else {
+            k.classList.remove('toggled')
         }
     })
+
+    const otherKey = key == 1 ? 2 : 1
+
     toggled[key] = !toggled[key]
+
+    if (toggled[otherKey]) {
+        for(const letter in toggleMap[otherKey]) {
+            toggleMap[otherKey][letter].classList.toggle('hidden')
+        }
+        toggled[otherKey] = false
+    }
+
     for(const letter in toggleMap[key]) {
         toggleMap[key][letter].classList.toggle('hidden')
     }
@@ -224,14 +319,25 @@ function makeGuess() {
     activeGuess.classList.remove('shake-animation')
     void activeGuess.offsetWidth
 
-    if (guessWord.length !== LETTER_COUNT || !wordList.includes(guessWord)) {
+    if (guessWord.length !== LETTER_COUNT || guessedWords.includes(guessWord) || !wordList.includes(guessWord)) {
         activeGuess.classList.add('shake-animation')
         return
     }
 
+    // Save game state
+    guessedWords.push(guessWord)
+    localData.gameState = guessedWords
+    saveLocalData()
+
     evaluateGuess()
 
-    nextGuess()
+    if (guessedWords.length === GUESS_COUNT && !gameOver) {
+        finishGame(false)
+    }
+
+    if (!gameOver) {
+        nextGuess()
+    }
 }
 
 function evaluateGuess() {
@@ -241,25 +347,25 @@ function evaluateGuess() {
     activeGuessLetters.forEach((guessLetter) => {
         letterIndex++
 
-        let letter = guessLetter.textContent.toLowerCase()
+        const letter = guessLetter.textContent.toLowerCase()
+        const keyboardKey = document.querySelector(`.keyboard-key[data-letter="${letter.toUpperCase()}"]`)
 
         // Incorrect letter
-        if (!wordOfTheDay.includes(letter)) {
-            let keyboardLetters = document.querySelectorAll(`.keyboard-key[data-letter="${letter.toUpperCase()}"]`)
-            keyboardLetters.forEach((letter) => {
-                letter.classList.add('incorrect')
-            })
+        if (!localData.wordOfTheDay.includes(letter)) {
+            keyboardKey.classList.add('incorrect')
             guessLetter.classList.add('incorrect')
             return
         }
 
         // Correct letter, wrong place
-        if (wordOfTheDay.charAt(letterIndex) != guessWord.charAt(letterIndex)) {
+        if (localData.wordOfTheDay.charAt(letterIndex) != guessWord.charAt(letterIndex)) {
+            keyboardKey.classList.add('half-correct')
             guessLetter.classList.add('half-correct')
             return
         }
 
         // Correct letter, correct place
+        keyboardKey.classList.add('correct')
         guessLetter.classList.add('correct')
         correctLetters++
     })
@@ -271,8 +377,24 @@ function evaluateGuess() {
 
 function finishGame(win) {
     gameOver = true
-    
-    toggleModal('game-over')
+    gameWon = win
+
+    if (!localData.finishedToday) {
+        if (win) {
+            localData.streak++
+        } else {
+            localData.streak = 0
+        }
+        localData.guessCount += guessedWords.length
+        localData.gameCount++
+        localData.finishedToday = true
+        saveLocalData()
+    }
+
+    // Set end stats
+    updateStatsModal()
+
+    toggleModal('stats')
 }
 
 
